cbbad7c4022d2f3fdb7ef47eeeca78467059146e
UpulieAdrt <35255575+UpulieAdrt@users.noreply.github.com>
2018-01-15 14:26:06 +0530

    Implemented the sqs to read

--- Test_U14/Test_U14-lambda.js
+++ Test_U14/Test_U14-lambda.js
@@ -1,8 +1,31 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback){
+let SL = require('slappforge-aws');
+const sqs = new SL.AWS.SQS();
+exports.handler = function (event, context, callback) {
 
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 	console.log("Triggered by the API_ANY");
+
+	sqs.receiveMessage({
+		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/480964559519/test-read',
+		AttributeNames: ['All'],
+		MaxNumberOfMessages: '1',
+		VisibilityTimeout: '30',
+		WaitTimeSeconds: '0'
+	}, function (receivedMessages) {
+		receivedMessages.forEach(message => {
+			// your logic to access each message through out the loop. Each message is available under variable message 
+			// within this block
+
+			console.log("Fetched the message");
+			callback(null,"read from the queue. ");
+		})
+	}, function (error) {
+		// implement error handling logic here
+
+			console.log("Failed to fetch the messages. No messages found");
+	});
+
 
 }
